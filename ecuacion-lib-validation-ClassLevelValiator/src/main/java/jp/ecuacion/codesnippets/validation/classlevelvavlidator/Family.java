1ae216edca49a5fba93255bc4a5fcce74c57ce06
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-03-08 00:06:28 +0900

    Updated

--- ecuacion-lib-validation-ClassLevelValiator/src/main/java/jp/ecuacion/codesnippets/validation/classlevelvavlidator/Family.java
+++ ecuacion-lib-validation-ClassLevelValiator/src/main/java/jp/ecuacion/codesnippets/validation/classlevelvavlidator/Family.java
@@ -2,7 +2,7 @@ package jp.ecuacion.codesnippets.validation.classlevelvavlidator;
 
 import jp.ecuacion.lib.validation.constraints.GreaterThanOrEqualTo;
 
-@GreaterThanOrEqualTo(propertyPath = "bigBrother.age", basisPropertyPath = "littleBrother.age",
+@GreaterThanOrEqualTo(propertyPath = "bigBrother.age", baselinePropertyPath = "littleBrother.age",
     message = "兄の年齢が弟より下です。")
 public record Family(Person bigBrother, Person littleBrother) {
 
