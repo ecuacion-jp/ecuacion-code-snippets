d07bc955a8d80860a9ea993b9055a55c8fe32dc2
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-03-14 09:39:37 +0900

    Updated

---
+++ ecuacion-lib-core-JakartaValidationItemName/src/main/java/jp/ecuacion/codesnippets/validation/jakartavalidationitemname/Main.java
@@ -0,0 +1,25 @@
+package jp.ecuacion.codesnippets.validation.jakartavalidationitemname;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.Set;
+import jp.ecuacion.lib.core.util.ExceptionUtil;
+
+public class Main {
+
+  private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+  public static void main(String... args) {
+    基本的な使い方();
+  }
+
+  public static void 基本的な使い方() {
+    Account account = new Account(null, null);
+
+    Set<ConstraintViolation<Account>> set = validator.validate(account);
+    for (String message : ExceptionUtil.getMessageList(set)) {
+      System.out.println(message);
+    }
+  }
+}
