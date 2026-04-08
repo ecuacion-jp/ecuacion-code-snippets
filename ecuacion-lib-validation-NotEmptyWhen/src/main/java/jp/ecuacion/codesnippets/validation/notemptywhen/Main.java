6be6b2ea6b58b2cf70bd83b88d1ca4d9eaea2969
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-04-09 07:42:37 +0900

    Updated

--- ecuacion-lib-validation-NotEmptyWhen/src/main/java/jp/ecuacion/codesnippets/validation/notemptywhen/Main.java
+++ ecuacion-lib-validation-NotEmptyWhen/src/main/java/jp/ecuacion/codesnippets/validation/notemptywhen/Main.java
@@ -3,8 +3,6 @@ package jp.ecuacion.codesnippets.validation.notemptywhen;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
-import java.util.Set;
-import jp.ecuacion.lib.core.util.ExceptionUtil;
 
 public class Main {
 
@@ -12,39 +10,14 @@ public class Main {
 
   public static void main(String... args) {
     基本的な使い方();
-    ValidationMessages_propertiesの使用();
-    ExceptionUtilを使用したメッセージ出力();
   }
 
   public static void 基本的な使い方() {
     AccountWithMessage account = new AccountWithMessage("John", true, null);
 
-    Set<ConstraintViolation<AccountWithMessage>> set = validator.validate(account);
+    var set = validator.validate(account);
     for (ConstraintViolation<?> v : set) {
       System.out.println(v.getMessage());
-    }
-  }
-
-  private static void ValidationMessages_propertiesの使用() {
-    AccountWithMessageKey account = new AccountWithMessageKey("John", true, null);
-
-    Set<ConstraintViolation<AccountWithMessageKey>> set = validator.validate(account);
-    for (ConstraintViolation<?> v : set) {
-      System.out.println(v.getMessage());
-    }
-  }
-
-  private static void ExceptionUtilを使用したメッセージ出力() {
-    Account account = new Account("John", true, null);
-
-    Set<ConstraintViolation<Account>> set = validator.validate(account);
-    for (String message : ExceptionUtil.getMessageList(set)) {
-      System.out.println(message);
-    }
-    
-    // 項目名を追加した場合
-    for (String message : ExceptionUtil.getMessageList(set, true)) {
-      System.out.println(message);
     }
   }
 }
