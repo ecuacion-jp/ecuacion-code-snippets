68d6e21d6d5ba2de872720c79b786cc093f2cfe4
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-04-09 07:40:51 +0900

    Updated

--- ecuacion-lib-validation-AnyNotEmpty/src/main/java/jp/ecuacion/codesnippets/validation/anynotempty/Main.java
+++ ecuacion-lib-validation-AnyNotEmpty/src/main/java/jp/ecuacion/codesnippets/validation/anynotempty/Main.java
@@ -4,7 +4,6 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import java.util.Set;
-import jp.ecuacion.lib.core.util.ExceptionUtil;
 
 public class Main {
 
@@ -12,8 +11,6 @@ public class Main {
 
   public static void main(String... args) {
     基本的な使い方();
-    ValidationMessages_propertiesの使用();
-    ExceptionUtilを使用したメッセージ出力();
   }
 
   public static void 基本的な使い方() {
@@ -22,24 +19,6 @@ public class Main {
     Set<ConstraintViolation<PersonWithMessage>> set = validator.validate(person);
     for (ConstraintViolation<?> v : set) {
       System.out.println(v.getMessage());
-    }
-  }
-
-  private static void ValidationMessages_propertiesの使用() {
-    PersonWithMessageKey person = new PersonWithMessageKey("John", null, "");
-
-    Set<ConstraintViolation<PersonWithMessageKey>> set = validator.validate(person);
-    for (ConstraintViolation<?> v : set) {
-      System.out.println(v.getMessage());
-    }
-  }
-
-  private static void ExceptionUtilを使用したメッセージ出力() {
-    Person person = new Person("", null, "");
-
-    Set<ConstraintViolation<Person>> set = validator.validate(person);
-    for (String message : ExceptionUtil.getMessageList(set)) {
-      System.out.println(message);
     }
   }
 }
--- ecuacion-lib-validation-AnyNotEmpty/src/main/java/jp/ecuacion/codesnippets/validation/anynotempty/PersonWithMessageKey.java
+++
@@ -1,9 +0,0 @@
-package jp.ecuacion.codesnippets.validation.anynotempty;
-
-import jp.ecuacion.lib.validation.constraints.AnyNotEmpty;
-
-@AnyNotEmpty(propertyPath = {"homePhoneNumber", "mobilePhoneNumber"}, 
-      message = "{eitherPhoneNumberRequired.message}")
-public record PersonWithMessageKey(String name, String homePhoneNumber, String mobilePhoneNumber) {
-
-}
